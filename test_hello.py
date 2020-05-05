2f72be08de8c9cfa8972b9b20471bd098e024bd5
Grace T <gracecircleci@gmail.com>
2020-05-04 21:41:34 -0700

    fix

--- test_hello.py
+++ test_hello.py
@@ -16,7 +16,9 @@ xml_string = ''' <?xml version="1.0" encoding="UTF-8"?>
 '''
 
 print('Current getcwd:', os.getcwd())
-with open('~/test-results/report.html', 'w+') as f:
+if not os.path.exists('test-results')
+  os.makedirs('test-results')
+with open('~/test-results/report.xml', 'w+') as f:
 
   f.write(xml_str)
 
