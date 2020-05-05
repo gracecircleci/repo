9815c03996af7e2f925bc905345d1cc81248e134
Grace T <gracecircleci@gmail.com>
2020-05-04 20:54:55 -0700

    fix html

--- test_hello.py
+++ test_hello.py
@@ -1,29 +1,15 @@
 html_string = '''
 <html>
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-        <style>body{ margin:0 100; background:whitesmoke; }</style>
-    </head>
     <body>
-        <h1>2014 technology and CPG stock prices</h1>
+        <h1> CICD Test Runs </h1>
 
         <!-- *** Section 1 *** --->
         <h2>Section 1: Apple Inc. (AAPL) stock in 2014</h2>
-        <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="no" \
-src="''' + first_plot_url + '''.embed?width=800&height=550"></iframe>
         <p>Apple stock price rose steadily through 2014.</p>
         
         <!-- *** Section 2 *** --->
         <h2>Section 2: AAPL compared to other 2014 stocks</h2>
-        <iframe width="1000" height="1000" frameborder="0" seamless="seamless" scrolling="no" \
-src="''' + second_plot_url + '''.embed?width=1000&height=1000"></iframe>
-        <p>GE had the most predictable stock price in 2014. IBM had the highest mean stock price. \
-The red lines are kernel density estimations of each stock price - the peak of each red lines \
-corresponds to its mean stock price for 2014 on the x axis.</p>
-        <h3>Reference table: stock tickers</h3>
-        ''' + summary_table_2 + '''
-        <h3>Summary table: 2014 stock statistics</h3>
-        ''' + summary_table_1 + '''
+        <p>GE had the most predictable stock price in 2014.  </p>
     </body>
 </html>'''
 with open('~/report.html', 'w') as f:
