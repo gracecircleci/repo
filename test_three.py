ae1148cdd4627753086fdc33911aea6af45bc144
Grace T <gracecircleci@gmail.com>
2020-05-05 23:09:26 -0700

    post http request

--- test_three.py
+++ test_three.py
@@ -1,3 +1,45 @@
-import os
+# catalog.py
+import os.path
+import json, time
+import urllib.request
+from urllib.parse import unquote, quote, urlparse
+from enum import Enum
+import pandas as pd
+from pandas import json_normalize
 
-assert True == True, "Error: test %s failed"  % __file__
+class CatalogEnum(Enum):
+    HOME = 1
+    SHOWS =2
+    MOVIES = 3
+    def __str__(self):
+        return '%s' % self.name
+
+class Const:
+    @property
+    def HEADERS(self):
+        epoch_time = int(time.time())
+
+        header_str = '''
+        {
+            "Content-Type": "application/json",
+            "LanguageIso2Code": "EN",
+            "CountryIso2Code": "US",
+            "ClientDateTimeSeconds": %d,
+            "SctvVersion": "5.0.0.0",
+            "ModelName": "E50-F2"  
+        }''' % epoch_time
+        #ToDo: "ModelName": "E50-F2" # is this needed
+        headersdict = json.loads(header_str)
+        return json.dumps(headersdict)
+
+if __name__ == '__main__':
+  url = 'http://catalog-dev.smartcasttv.com/catalogs?rowsToExpand=3' 
+  headers = json.loads(Const().HEADERS)
+  payload_str = '{ "Catalogs":[{ "Id" : 1 }]}'
+  data = json.loads(payload_str)
+  data = json.dumps(data)
+  data = str.encode(data)
+  req = urllib.request.Request(url, headers=headers)
+  response = urllib.request.urlopen(req, data)
+  print('response status=', response.status)
+  assert response.status==200
